8a35bdb9e3036c1f8f81e64ab7469940b3c469a8
Pe4enkazAMI <yavmaksimov@edu.hse.ru>
2023-11-06 17:21:30 +0300

    fixes

--- asr_project_template/hw_ss/utils/parse_config.py
+++ asr_project_template/hw_ss/utils/parse_config.py
@@ -134,15 +134,6 @@ class ConfigParser:
         logger.setLevel(self.log_levels[verbosity])
         return logger
 
-    def get_text_encoder(self) -> BaseTextEncoder:
-        if self._text_encoder is None:
-            if "text_encoder" not in self._config:
-                self._text_encoder = CTCCharTextEncoder()
-            else:
-                self._text_encoder = self.init_obj(self["text_encoder"],
-                                                   default_module=text_encoder_module)
-        return self._text_encoder
-
     # setting read-only attributes
     @property
     def config(self):
